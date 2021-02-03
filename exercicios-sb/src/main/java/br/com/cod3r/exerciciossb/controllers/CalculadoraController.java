590a3be6d291d5d02129efce17a55d51521caabf
AquilesDias <kilesax@gmail.com>
2021-02-03 14:17:24 -0300

    Web service calculadora

---
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/CalculadoraController.java
@@ -0,0 +1,25 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/calculadora")
+public class CalculadoraController {
+	
+	@GetMapping(path="/somar/{a}/{b}")
+	public int somar (@PathVariable int a, @PathVariable int b) {
+		return a + b;
+	}
+	
+	// calculadora/subtrair?a=2&b=2
+	@GetMapping(path="/subtrair")
+	public int subtrair(@RequestParam(name="a") int a, 
+			            @RequestParam(name="b") int b) {
+		return a-b;
+	}
+
+}
