429f075dc622f5c691220791242f249ec0996765
AquilesDias <kilesax@gmail.com>
2021-02-03 11:35:13 -0300

    Passando paramentros web service

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class ClienteController {
 	@GetMapping(path="/dados")
 	public Cliente obterCliente() {
 		return new Cliente(2, "Aquiles", "541.215.454-00");
+	}
+	
+	@GetMapping(path="/{id}")
+	public Cliente obterClientePorId(@PathVariable int id) {
+		return new Cliente(id, "Barbara Alves", "781.268.879-89");
 	}
 }
