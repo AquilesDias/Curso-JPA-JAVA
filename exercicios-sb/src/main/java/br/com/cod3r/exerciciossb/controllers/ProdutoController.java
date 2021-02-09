ebccbf0c50d9efb4ca4e8cecebb2d26e1ac13307
AquilesDias <kilesax@gmail.com>
2021-02-09 02:00:35 -0300

    excluindo produto por id

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -5,10 +5,9 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -45,5 +44,10 @@ public class ProdutoController {
 //	public Produto alterarProduto(@Valid Produto produto) {
 //		return produtoRepository.save(produto);
 //	}
+	
+	@DeleteMapping(path="/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		 produtoRepository.deleteById(id);
+	}
 
 }
